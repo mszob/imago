fbfe00f6bfc4ba6abec70c8324089b493a7490ea
mszob <72834440+mszob@users.noreply.github.com>
2022-02-08 22:51:20 -0600

    fav. no pref. fix 6

--- script.js
+++ script.js
@@ -21,20 +21,19 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 })
 
-$(document).ready(function() {
-    if (!window.matchMedia)
-        return;
+matcher = window.matchMedia('(prefers-color-scheme: dark)');
+matcher.addListener(onUpdate);
+onUpdate();
 
-    var current = $('head > link[rel="icon"][media]');
-    $.each(current, function(i, icon) {
-        var match = window.matchMedia(icon.media);
-        function swap() {
-            if (match.matches) {
-                current.remove();
-                current = $(icon).appendTo('head');
-            }
-        }
-        match.addEventListener('change', swap);
-        swap();
-    });
-});
+lightSchemeIcon = document.querySelector('link#light-scheme-icon');
+darkSchemeIcon = document.querySelector('link#dark-scheme-icon');
+
+function onUpdate() {
+  if (matcher.matches) {
+    lightSchemeIcon.remove();
+    document.head.append(darkSchemeIcon);
+  } else {
+    document.head.append(lightSchemeIcon);
+    darkSchemeIcon.remove();
+  }
+}
