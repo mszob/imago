208810e20765625aafd81bddc0c54827b0bcb041
picturamundi <72834440+picturamundi@users.noreply.github.com>
2022-04-07 14:30:28 -0500

    dynamic favicon

--- script.js
+++ script.js
@@ -1,39 +1,29 @@
 // this one is jut to wait for the page to load
 document.addEventListener('DOMContentLoaded', () => {
     const themeStylesheet = document.getElementById('theme');
+    const faviconTheme = document.getElementById('favicon');
     const storedTheme = localStorage.getItem('theme');
+    const storedFavicon = localStorage.getItem('favicon');
     if(storedTheme){
         themeStylesheet.href = storedTheme;
     }
+    if(storedFavicon){
+       faviconTheme.href = storedFavicon;
+  }
     const themeToggle = document.getElementById('theme-toggle');
     themeToggle.addEventListener('click', () => {
-        // if it's light -> go dark
         if(themeStylesheet.href.includes('light')){
             themeStylesheet.href = 'dark.css';
-            //themeToggle.innerText = 'Switch to light mode';
+            faviconTheme.href = 'favicon/dark.svg';
         } else {
-            // if it's dark -> go light
             themeStylesheet.href = 'light.css';
-            //themeToggle.innerText = 'Switch to dark mode';
+            faviconTheme.href = 'favicon/light.svg';
         }
-        // save the preference to localStorage
         localStorage.setItem('theme',themeStylesheet.href)
+        localStorage.setItem('favicon',faviconTheme.href)
     })
 })
 
 matcher = window.matchMedia('(prefers-color-scheme: dark)');
 matcher.addListener(onUpdate);
 onUpdate();
-
-lightSchemeIcon = document.querySelector('link#light-scheme-icon');
-darkSchemeIcon = document.querySelector('link#dark-scheme-icon');
-
-function onUpdate() {
-  if (matcher.matches) {
-    lightSchemeIcon.remove();
-    document.head.append(darkSchemeIcon);
-  } else {
-    document.head.append(lightSchemeIcon);
-    darkSchemeIcon.remove();
-  }
-}
