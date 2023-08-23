013ac64e4bfbf61f61b13d29d7354ed7c3110c56
picturamundi <mszobody@protonmail.com>
2023-08-23 13:54:03 -0400

    (main-col.expand position:abs)

--- script.js
+++ script.js
@@ -106,19 +106,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // custom back arrow
 
-// (function (window, location) {
-//     history.replaceState(null, document.title, location.pathname + "#!/stealingyourhistory");
-//     history.pushState(null, document.title, location.pathname);
-//     window.addEventListener("popstate", function () {
-//         if (location.hash === "#!/stealingyourhistory") {
-//             history.replaceState(null, document.title, location.pathname);
-//             setTimeout(function () {
-//                 location.replace("index.html");
-//             }, 0);
-//         }
-//     }, false);
-// }(window, location));
-
 window.onpopstate = function () {
     home();
 }; history.pushState({}, '');
